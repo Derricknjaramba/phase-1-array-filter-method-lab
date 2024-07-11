b2d2a06cf79d6da366222d78078d4cceeb758993
Derricknjaramba <njarambaderrick@gmail.com>
2024-07-11 08:23:27 +0300

    filter

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+const drivers=[
+  {name:'Sally' ,hometown:'New York'},
+   {name:'Bob' ,hometown:'Los Angeles'},
+    {name:'Alice' ,hometown:'San Fransico'},
+     {name:'Derek' ,hometown:'Seattle'},
+      {name:'Samantha' ,hometown:'New York'},
+
+]
+
+function findMatching(drivers,name){
+  return drivers.filter(driver=>
+    driver.toLowerCase()==name.toLowerCase());
+}
+
+function fuzzyMatch(drivers,letters){
+  return drivers.filter(driver=>
+    driver.toLowerCase().startsWith(letters.toLowerCase())
+);
+}
+ function matchName(drivers,name){
+  return drivers.filter(driver=>
+    driver.name.toLowerCase()===name.toLowerCase()
+);
+ }
+
+ console.log("Drivers matching 'Sally':",findmatching(['sally','Bob','Alice','Derek','Samatha'],'Sally'));
+  console.log("Drivers with names starting with 'Sa':",fuzzyMatch(['sally','Bob','Alice','Derek','Samatha'],'Sa'));
+  console.log("Driver object matching 'Sally':",matchName(drivers,'Sally'));
